a8ae4b3a3120feef7470ccc0ab158cd0bb8ec024
glowfi <86939591+glowfi@users.noreply.github.com>
2021-07-06 18:46:15 +0530

    Added some features

--- toggle.sh
+++ toggle.sh
@@ -9,12 +9,14 @@ if [ $current = "true" ]; then
   kwriteconfig5 --file kwinrc --group Plugins --key diminactiveEnabled false
   kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key Enabled false
   qdbus org.kde.plasmashell /PlasmaShell evaluateScript "p = panelById(panelIds[0]); p.location = 'bottom';p.height = 44;"
+  sed -i '16s/.*/echo $XDG_CURRENT_DESKTOP/' .local/bin/fm/fetchmaster.sh
   notify-send 'Normal Mode'
 elif [ $current = "false" ]; then
   kwriteconfig5 --file kwinrc --group Plugins --key krohnkiteEnabled true
   kwriteconfig5 --file kwinrc --group Plugins --key diminactiveEnabled true
   kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key Enabled true
   qdbus org.kde.plasmashell /PlasmaShell evaluateScript "p = panelById(panelIds[0]); p.location = 'top';p.height = 25;"
+  sed -i '16s/.*/echo $XDG_CURRENT_DESKTOP\/Krohnkite /' .local/bin/fm/fetchmaster.sh
   notify-send 'Tiling Mode'
 fi
 
