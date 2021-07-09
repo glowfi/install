5629f842f01562ecbbf1e03c792b90785d0324be
glowfi <86939591+glowfi@users.noreply.github.com>
2021-07-09 13:40:00 +0530

    Added fixes

--- configs/ranger/commands.py
+++ configs/ranger/commands.py
@@ -5,10 +5,12 @@ class fzf_searchFile(Command):
     """
     Find a file using fzf.
     """
+
     def execute(self):
         import subprocess
         import os.path
-        fzf = self.fm.execute_command("du -a ~/.config/* ~/install/* ~/cdx/* ~/main/* | awk '{print $2}' |fzf --preview 'bat --style numbers,changes --color=always {}'", universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(
+            "du -a ~/.config/* ~/cdx/* ~/main/* | awk '{print $2}' |fzf --preview 'bat --style numbers,changes --color=always {}'", universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
@@ -17,14 +19,17 @@ class fzf_searchFile(Command):
             else:
                 self.fm.select_file(fzf_file)
 
+
 class fzf_searchContent(Command):
     """
     Find a contents inside of file using fzf.
     """
+
     def execute(self):
         import subprocess
         import os.path
-        fzf = self.fm.execute_command("rg . | fzf | awk -F':' '{ print $1 }'", universal_newlines=True, stdout=subprocess.PIPE)
+        fzf = self.fm.execute_command(
+            "rg . | fzf | awk -F':' '{ print $1 }'", universal_newlines=True, stdout=subprocess.PIPE)
         stdout, stderr = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
