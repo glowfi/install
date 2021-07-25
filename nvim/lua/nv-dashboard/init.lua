8152b5ac3dd6f1bbbb2361b809a07a16469f2ee8
glowfi <86939591+glowfi@users.noreply.github.com>
2021-07-25 19:53:35 +0530

    Fix

--- nvim/lua/nv-dashboard/init.lua
+++ nvim/lua/nv-dashboard/init.lua
@@ -1,15 +1,32 @@
+-- Dashboard
+
 -- CUSTOM HEADER
-local custom_header = {
- ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
- ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
- ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
- ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
- ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
- ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+local custom_header={
+'               ,   ,                             ',
+'               $,  $,     ,                      ',
+'               "ss.$ss. .s"                      ',
+'       ,     .ss$$$$$$$$$$s,                     ',
+'       $. s$$$$$$$$$$$$$$`$$Ss                   ',
+'       "$$$$$$$$$$$$$$$$$$o$$$       ,           ',
+'      s$$$$$$$$$$$$$$$$$$$$$$$$s,  ,s            ',
+'     s$$$$$$$$$"$$$$$$""""$$$$$$"$$$$$,          ',
+'     s$$$$$$$$$$s""$$$$ssssss"$$$$$$$$"          ',
+'    s$$$$$$$$$$"         `"""ss"$"$s""           ',
+'    s$$$$$$$$$$,              `"""""$  .s$$s     ',
+'    s$$$$$$$$$$$$s,...               `s$$"  `    ',
+'`ssss$$$$$$$$$$$$$$$$$$$$####s.     .$$"$.   , s-',
+'  `""""$$$$$$$$$$$$$$$$$$$$#####$$$$$$"     $.$" ',
+'        "$$$$$$$$$$$$$$$$$$$$$####s""     .$$$|  ',
+'         "$$$$$$$$$$$$$$$$$$$$$$$$##s    .$$" $  ',
+'          $$""$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"   ` ',
+'         $$"  "$"$$$$$$$$$$$$$$$$$$$$S"""""      ',
+'    ,   ,"     $  $$$$$$$$$$$$$$$$####s          ',
+'    $.          .s$$$$$$$$$$$$$$$$$####"         ',
+'    "$sssssssssS$$$$$$$$$$$$$$$$$$$####"         ',
 }
 
 -- CUSTOM FOOTER
-local custom_footer= {''}
+local custom_footer= {'Welcome to Dragon Inn!'}
 
 -- SETTING CUSTOM HEADER AND FOOTER
 vim.g.dashboard_custom_header=custom_header
@@ -20,12 +37,5 @@ vim.g.dashboard_default_executive = 'telescope'
 vim.g.dashboard_custom_section = {
     a = {description = {'  Find File          '}, command = 'Telescope find_files'},
     b = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    c = {description = { "  Recently Used Files" },command = "Telescope oldfiles",},
-    d = {description = {'  Open Folder        '}, command = ':RnvimrToggle'},
-    e = {description = {'  Load Last Session  '}, command = ':SessionLoad last'},
-    f = {description = {'  Settings           '}, command = ':e '..CONFIG_PATH..'/init.lua'},
-    g = {description = {'  Close              '}, command = ':qall'},
+    c = {description = {'  Recently Used Files'}, command = "Telescope oldfiles",},
     }
-
--- Session Keymappings
-vim.cmd("nmap <S-q> :SessionSave last<CR>")
