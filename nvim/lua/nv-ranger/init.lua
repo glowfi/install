faa9daa8d17d4242eb37a67c6548cdb2cecc1bb3
glowfi <86939591+glowfi@users.noreply.github.com>
2021-08-04 03:36:05 +0530

    fixes

--- nvim/lua/nv-ranger/init.lua
+++ nvim/lua/nv-ranger/init.lua
@@ -3,3 +3,9 @@ vim.cmd("let g:rnvimr_ex_enable = 1")
 
 -- Toggle ranger
 vim.cmd('nnoremap <F3> :RnvimrToggle<CR>')
+
+-- Make Neovim wipe the buffers corresponding to the files deleted by Ranger
+vim.cmd"let g:rnvimr_enable_bw = 1"
+
+-- Link CursorLine into RnvimrNormal highlight in the Floating window
+vim.cmd"highlight link RnvimrNormal CursorLine"
