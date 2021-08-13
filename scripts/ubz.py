ab01085822190239cf4ac5eaf25bea9b93879f1c
glowfi <86939591+glowfi@users.noreply.github.com>
2021-08-13 16:02:04 +0530

    Minor Updatev1

--- scripts/ubz.py
+++ scripts/ubz.py
@@ -2,7 +2,7 @@ import ueberzug.lib.v0 as ueberzug
 import time
 
 with ueberzug.Canvas() as c:
-    paths = ["/home/ayush/main/1.jpg"]
+    paths = [""]
     demo = c.create_placement("demo", x=0, y=0, height=50, width=50)
     demo.path = paths[0]
     demo.visibility = ueberzug.Visibility.VISIBLE
