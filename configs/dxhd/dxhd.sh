ac3a23c5a1c2b8d55355cb51a31ff9241694766d
glowfi <86939591+glowfi@users.noreply.github.com>
2021-08-15 18:14:03 +0530

    cleaned up

--- configs/dxhd/dxhd.sh
+++ configs/dxhd/dxhd.sh
@@ -1,4 +1,4 @@
-#!/bin/fish
+#!/bin/sh
 
 ### Plasma Bindings
 
@@ -24,7 +24,23 @@
 
 ## Toggle Tiling/Floating mode
 #alt + t
-    cp -r ~/install/scripts/toggle.sh ~;chmod +x toggle.sh;./toggle.sh;rm -rf ~/toggle.sh;
+    current=`kreadconfig5 --file kwinrc --group Plugins --key krohnkiteEnabled`
+
+    if [ $current = "true" ]; then
+    kwriteconfig5 --file kwinrc --group Plugins --key krohnkiteEnabled false
+    kwriteconfig5 --file kwinrc --group Plugins --key diminactiveEnabled false
+    kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key Enabled false
+    qdbus org.kde.plasmashell /PlasmaShell evaluateScript "p = panelById(panelIds[0]); p.location = 'bottom';p.height = 44;"
+    notify-send 'Normal Mode'
+
+    elif [ $current = "false" ]; then
+    kwriteconfig5 --file kwinrc --group Plugins --key krohnkiteEnabled true
+    kwriteconfig5 --file kwinrc --group Plugins --key diminactiveEnabled true
+    kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key Enabled true
+    qdbus org.kde.plasmashell /PlasmaShell evaluateScript "p = panelById(panelIds[0]); p.location = 'top';p.height = 25;"
+    notify-send 'Tiling Mode'
+    fi
+    qdbus org.kde.KWin /KWin reconfigure
 
 ### Global bindings
 
