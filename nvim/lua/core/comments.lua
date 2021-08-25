5b8b4af0a05192efc42ea68153ad5266360875a4
glowfi <86939591+glowfi@users.noreply.github.com>
2021-08-25 10:30:53 +0530

    added

--- nvim/lua/core/comments.lua
+++ nvim/lua/core/comments.lua
@@ -1,4 +1,3 @@
 -- Keymappings
 require('nvim_comment').setup({comment_empty = false})
 vim.api.nvim_set_keymap('n', '<C-_>', ":CommentToggle<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<C-_>', ":CommentToggle<CR>", {noremap = true, silent = true})
