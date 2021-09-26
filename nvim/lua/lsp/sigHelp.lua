62ab0d8d96c40181fecdb03b171b0e6301bd6364
glowfi <86939591+glowfi@users.noreply.github.com>
2021-09-26 15:44:37 +0530

    "minor fix"

--- nvim/lua/lsp/sigHelp.lua
+++ nvim/lua/lsp/sigHelp.lua
@@ -6,7 +6,7 @@
   floating_window = true,
   fix_pos = false,
   hint_enable = true,
-  hint_prefix = "🐻 ",
+  hint_prefix = "",
   hint_scheme = "String",
   hi_parameter = "Search",
   max_height = 10,
