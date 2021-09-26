285729f72cd6bd138b5d0a657f288100a73f8be9
glowfi <86939591+glowfi@users.noreply.github.com>
2021-09-26 15:42:29 +0530

    "major update"

--- legacy/ressurect-ranger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# COPY RANGER FM SETTINGS
-cp -r ~/install/legacy/ranger ~/.config
-git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
