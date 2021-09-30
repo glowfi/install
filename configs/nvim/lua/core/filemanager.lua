c6a83de565c684a1d1d0342b4bc1fed7b2c821b4
glowfi <86939591+glowfi@users.noreply.github.com>
2021-09-30 18:29:09 +0530

    minor fixes

--- configs/nvim/lua/core/filemanager.lua
+++ configs/nvim/lua/core/filemanager.lua
@@ -9,4 +9,4 @@ require("nnn").setup({
 vim.cmd "let g:nnn#layout = { 'window': { 'width': 0.6, 'height': 0.6, 'highlight': 'Debug' } }"
 
 -- Keymappings
-vim.cmd "nnoremap <leader>nn :NnnPicker %:p:h<CR>"
+vim.cmd "nnoremap <silent><leader>nn :NnnPicker %:p:h<CR>"
