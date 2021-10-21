211ab08befe28f76ba8cbf119104b7c0ee6d1757
glowfi <86939591+glowfi@users.noreply.github.com>
2021-10-21 20:34:58 +0530

    removed

--- scripts/krohnkite.sh
+++ scripts/krohnkite.sh
@@ -21,38 +21,3 @@ kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key ExceptionPatte
 kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key ExceptionType 0
 kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key HideTitleBar true
 kwriteconfig5 --file breezerc --group "Windeco Exception 0" --key Mask 16
-
-# Install Forceblur
-cp -r ~/install/configs/forceblur-0.5.kwinscript ~
-plasmapkg2 -i ~/forceblur-0.5.kwinscript || plasmapkg2 -u ~/forceblur-0.5.kwinscript
-rm -rf ~/forceblur-0.5.kwinscript
-
-# Copy forceblur.desktop
-mkdir -p ~/.local/share/kservices5/
-cp ~/.local/share/kwin/scripts/forceblur/metadata.desktop ~/.local/share/kservices5/forceblur.desktop
-
-# Update kwinrc
-kwriteconfig5 --file kwinrc --group Plugins --key blurEnabled true
-kwriteconfig5 --file kwinrc --group Plugins --key forceblurEnabled true
-kwriteconfig5 --file kwinrc --group Script-forceblur --key patterns kitty
-kwriteconfig5 --file kwinrc --group Effect-Blur --key BlurStrength 10
-qdbus org.kde.KWin /KWin reconfigure
-
-# INSTALL DEPENDENCIES
-sudo pacman -S --noconfirm gst-libav phonon-qt5-gstreamer gst-plugins-good qt5-quickcontrols qt5-graphicaleffects qt5-multimedia
-
-# LOCKSCREEN THEME
-mkdir -p ~/.local/share/plasma/wallpapers/
-git clone https://github.com/halverneus/org.kde.video
-mv org.kde.video ~/.local/share/plasma/wallpapers/
-git clone https://github.com/3ximus/aerial-sddm-theme
-cp -r aerial-sddm-theme/playlists ~
-rm -rf aerial-sddm-theme
-
-# ADDED TO CLIPBOARD
-sed 's:^#\(.*\)$:\1:g' ~/install/scripts/krohnkite.sh | tail -3 | xclip
-nvim -c ".+57" ~/.local/share/plasma/wallpapers/org.kde.video/contents/ui/main.qml
-
-# for (var k = 0; k < Math.ceil(Math.random() * 10) ; k++) {
-#     playlist.shuffle()
-# }
