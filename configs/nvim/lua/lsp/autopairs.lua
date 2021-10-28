c0ea9e93a73b6dc9ee3e6bfe005c38ed73bbc237
glowfi <86939591+glowfi@users.noreply.github.com>
2021-10-28 16:07:08 +0530

    addedv2

--- configs/nvim/lua/lsp/autopairs.lua
+++ configs/nvim/lua/lsp/autopairs.lua
@@ -3,10 +3,9 @@ local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 
 -- cmp based autopairs
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true,
-  map_complete = true
-})
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done())
 
 npairs.setup({
     check_ts = true,
